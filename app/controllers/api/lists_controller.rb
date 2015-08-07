738d2dd762666c7f0b24b62618cc26f99b67f85a
emilycw <emily.rose.cw@gmail.com>
2015-08-07 11:40:41 -0700

    Make lists controller

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,2 +1,24 @@
 class ListsController < ApplicationController
+  def show
+    list = List.find(params[:id])
+    render :json => list, :include => :cards
+  end
+
+  def index
+    render :json => List.all
+  end
+
+  def create
+    list = List.new(card_params)
+    if list.save!
+      render :json => list
+    else
+      render :json =>list.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def card_params
+    params.require(:list).permit(:name)
+  end
+
 end
