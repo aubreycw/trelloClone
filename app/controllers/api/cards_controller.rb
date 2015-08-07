2069e1f2850c9e19d93086efbaa331b8b692b76e
emilycw <emily.rose.cw@gmail.com>
2015-08-07 11:38:19 -0700

    Make cards controller

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,2 +1,23 @@
 class CardsController < ApplicationController
+    def show
+    card = Card.find(params[:id])
+    render :json => card, :include => :cards
+  end
+
+  def index
+    render :json => Card.all
+  end
+
+  def create
+    card = Card.new(card_params)
+    if card.save!
+      render :json => card
+    else
+      render :json => Card.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def card_params
+    params.require(:card).permit(:name)
+  end
 end
