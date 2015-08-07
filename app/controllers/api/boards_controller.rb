f5deb956d9718fcc6b67510892a879c4ffcd7d24
emilycw <emily.rose.cw@gmail.com>
2015-08-07 11:37:05 -0700

    Make boards controller

---
+++ app/controllers/api/boards_controller.rb
@@ -0,0 +1,23 @@
+class BoardsController < ApplicationController
+  def show
+    board = Board.find(params[:id])
+    render :json => board, :include => :cards
+  end
+
+  def index
+    render :json => Board.all
+  end
+
+  def create
+    board = Board.new(board_params)
+    if board.save!
+      render :json => board
+    else
+      render :json => board.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def board_params
+    params.require(:board).permit(:name)
+  end
+end
