008b03e54a1ec14b755575e61b105e76d3fe528b
emilycw <emily.rose.cw@gmail.com>
2015-08-07 11:39:59 -0700

    Make cards controller

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,7 +1,7 @@
 class CardsController < ApplicationController
-    def show
+  def show
     card = Card.find(params[:id])
-    render :json => card, :include => :cards
+    render :json => card, :include => :todo_items
   end
 
   def index
