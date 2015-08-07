39639b39049d5786d8a21d053b5ece03dac8381c
emilycw <emily.rose.cw@gmail.com>
2015-08-07 11:43:00 -0700

    Make todo items controller

--- app/controllers/api/todo_items_controller.rb
+++ app/controllers/api/todo_items_controller.rb
@@ -1,2 +1,23 @@
 class TodoItemsController < ApplicationController
+  def show
+    todo_item = TodoItem.find(params[:id])
+    render :json => todo_item
+  end
+
+  def index
+    render :json => TodoItem.all
+  end
+
+  def create
+    todo_item = TodoItem.new(todo_params)
+    if todo_item.save!
+      render :json => todo_item
+    else
+      render :json =>todo_item.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def todo_params
+    params.require(:todo_item).permit(:name)
+  end
 end
